f46a3386f3d3ab8aea93ee86951698577acb49fd
norbertmarko <norbertmarko92@gmail.com>
2019-06-10 15:00:04 +0200

    hdf5 writer class is full functional now

--- data_to_hdf5.py
+++ data_to_hdf5.py
@@ -1,41 +1,38 @@
-# dims(.shape) - (600, 32, 32, 3)
-# dims for mask?
-# outputPath - where we store the hdf5 file
-# datakey - name of the dataset - just for indication
-# buffSize - size of in-memory buffer (1000 images/feature vectors)
-import os
 import h5py
+import os
 
-class data_to_hdf5:
-    def __init__(self, img_dims, label_dims, outputPath, dataKey="images", buffSize=1000):
-        # value error can be raised here if path already exists
+class HDF5writer:
+    def __init__(self, img_dims, label_dims, outputPath, buffSize=1000):
         if os.path.exists(outputPath):
-            pass
-        # open HDF5 database for writing("w")
-        # create two datasets - images, labels(masks)
-        self.db = h5py.File(outputPath, "w")
-        self.data = self.db.create_dataset(dataKey, img_dims, dtype="float")
-        # label dims might need to be changed (from rgb(3) to num_classes) + dtype?
-        self.labels = self.db.create_dataset("labels", label_dims, dtype="float")
+            raise ValueError("The supplied ‘outputPath‘ already "
+            "exists and cannot be overwritten. Manually delete "
+            "the file before continuing.", outputPath)
 
+        # Database
+        self.db = h5py.File(outputPath, "w")
+        # Datasets
+        self.images = self.db.create_dataset("images", img_dims, dtype="float") #uint8?
+        self.labels = self.db.create_dataset("labels", label_dims, dtype="float")
+        # Buffer
         self.buffSize = buffSize
-        self.buffer ={"data": [], "labels": []}
+        self.buffer = {"images": [], "labels": []}
         self.idx = 0
 
-    #'add' function adding data to buffer extend(): Extends list by appending elements from the iterable.
     def add(self, rows, labels):
-        self.buffer["data"].extend(rows)
-        self.buffer["labels"].extend(labels) # rows - labels?
+        self.buffer["images"].extend(rows)
+        self.buffer["labels"].extend(labels)
+
+        if len(self.buffer["images"]) >= self.buffSize:
+            self.flush()
 
     def flush(self):
-        i = self.idx + len(self.buffer["data"]) # determine next available row
-        self.data[self.idx:i] = self.buffer["data"]
-        self.labels[self.idx:i] = self.buffer["labels"] # apply numpy slicing to store data+label
+        i = self.idx + len(self.buffer["images"]) # -1 ?
+        self.images[self.idx:i] = self.buffer["images"]
+        self.labels[self.idx:i] = self.buffer["labels"]
         self.idx = i
-        self.buffer = {"data":[], "labels":[]} # buffer reset
+        self.buffer = {"images": [], "labels": []}
 
-    # check for remaining data + close
     def close(self):
-        if len(self.buffer["data"]) > 0:
+        if len(self.buffer["images"]) > 0:
             self.flush()
-            self.db.close()
+        self.db.close()
